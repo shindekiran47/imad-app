c20c29011f5fb8c8652706f7545f3f82b21ab92e
shindekiran47 <30953352+shindekiran47@users.noreply.github.com>
2017-08-12 19:21:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'artical-one'));
+  res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 
 
