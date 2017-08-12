66dc0da6903911ce5ea7dc9498db796290588901
shindekiran47 <30953352+shindekiran47@users.noreply.github.com>
2017-08-12 19:44:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/artical-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
