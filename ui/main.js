160e386c2aab019dc8367694356ff40889839309
shindekiran47 <30953352+shindekiran47@users.noreply.github.com>
2017-08-26 14:39:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,30 @@ var counter = 0;
 
 button.onclick = function () {
   
+    //create a request object
+  var request = new XMLHttpRequest();
+  
+  //capure the response and store it in a variable
+  request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // tack some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+        //not yet done
+    };
+    // make a request
+    request.open('GET', 'http://jackyshinde47.imad.hasura-app.io/counter', true);
+    request.send(null);
+
   //for counter only
-  counter = counter + 10;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString(); 
+ // counter = counter + 10;
+ // var span = document.getElementById('count');
+ // span.innerHTML = counter.toString(); 
 };
   /*
   //create a request object
