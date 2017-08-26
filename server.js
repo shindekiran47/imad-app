baa50420a590a95f7f36101f4c218ca8409cfbcd
shindekiran47 <30953352+shindekiran47@users.noreply.github.com>
2017-08-26 13:42:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+appp.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:articalName', function (req, res) {
     //articalName == artical-one
     var articalName = req.params.articalName;
